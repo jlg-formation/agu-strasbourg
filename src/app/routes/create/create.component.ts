6f7bcd42d407ff29e802ae3e0a164000ea80e2a4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-15 12:06:07 +0200

    ok

--- src/app/routes/create/create.component.ts
+++ src/app/routes/create/create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elt: ElementRef) { }
 
   ngOnInit() {
+    this.elt.nativeElement.querySelector('input[name=name]').focus();
   }
 
 }
