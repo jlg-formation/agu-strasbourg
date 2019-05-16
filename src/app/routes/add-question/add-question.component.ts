59774d1fbe1029249b02478b2a774603e394bbfb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-16 16:22:17 +0200

    ok

--- src/app/routes/add-question/add-question.component.ts
+++ src/app/routes/add-question/add-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-question',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-question.component.scss']
 })
 export class AddQuestionComponent implements OnInit {
+
+  f = new FormGroup({});
 
   constructor() { }
 
