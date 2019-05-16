f1a988feda844c10ca473832d82a7ad0c26500a6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-16 16:33:40 +0200

    ok

--- src/app/routes/add-question/add-question.component.ts
+++ src/app/routes/add-question/add-question.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-question',
@@ -8,7 +8,14 @@ import { FormGroup } from '@angular/forms';
 })
 export class AddQuestionComponent implements OnInit {
 
-  f = new FormGroup({});
+  f = new FormGroup({
+    text: new FormControl('Quelle est la capitale de ...', Validators.required),
+    answer1: new FormControl('Paris', Validators.required),
+    answer2: new FormControl('Lyon', Validators.required),
+    answer3: new FormControl('Strasbourg', Validators.required),
+    answer4: new FormControl('Nancy', Validators.required),
+    goodAnswer: new FormControl('B', Validators.required),
+  });
 
   constructor() { }
 
